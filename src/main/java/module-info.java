ef44a740da27bff3c65d9c42cd993a9f693f56f7
RazviTNT <101121217+RazviTNT@users.noreply.github.com>
2022-05-20 00:06:34 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proiect_fis {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proiect_fis to javafx.fxml;
-    exports com.example.proiect_fis;
-}
